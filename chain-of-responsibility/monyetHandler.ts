3797b693aeefb87dcb4ed987a69f7c0b7de6f596
VArtz <farrelnikoson@gmail.com>
2025-03-15 21:19:02 +0700

    chain of responsibility (CoR) pattern

--- chain-of-responsibility/monyetHandler.ts
+++ chain-of-responsibility/monyetHandler.ts
@@ -2,6 +2,7 @@ import AbstractHandler from "./abstractHandler";
 
 export class MonyetHandler extends AbstractHandler {
     handle(request: string): string {
+        console.log(`   ${request} telah sampai ditangan monyet.`)
         if (request === 'pisang') {
             return `Monyet: saya akan makan ${request}nya.`
         }
